4cc56ed5fd02e78607dab1ea53fc324ce2423e9c
DrGorge <drobeng68@gmail.com>
2025-02-11 14:17:11 +0300

    Initial commit

---
+++ grosery_store.py
@@ -0,0 +1,18 @@
+from tkinter.font import names
+
+
+class Grosery_store():
+    def __init__(self,name,adress,items):
+        self.name = name
+        self.adress = adress
+        self.product = product
+        self.castom = castom
+        self.items = item('product':castom)
+        self.quantity = quantity
+
+   def add_items(self):
+        self.add_product= input(f"Введите наименование поступившего продукта:)
+        self.prouct += add_product
+
+    def
+
